47ec34516707e775290d5a22e387bca8089cbb4d
moura1001 <gmouraneto01@gmail.com>
2022-02-03 15:48:50 -0300

    primeiro commit com configuração inicial do projeto

---
+++ src/main/java/com/example/vegdog/VegDogApplication.java
@@ -0,0 +1,13 @@
+package com.example.vegdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegDogApplication.class, args);
+	}
+
+}
