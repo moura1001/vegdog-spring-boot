6cd2eb48ff4d0aea2de2ae646dd890020aa4c161
moura1001 <gmouraneto01@gmail.com>
2022-02-03 16:40:19 -0300

    commit com adição das classes de domínino (entidades JPA)

---
+++ src/main/java/com/example/vegdog/model/entity/Item.java
@@ -0,0 +1,50 @@
+package com.example.vegdog.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Length(min=3, max=32)
+	private String nome;
+	
+	@NotNull
+	@Min(Long.MIN_VALUE)
+	private Double preco;
+	
+	public Item() {}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
---
+++ src/main/java/com/example/vegdog/model/entity/Pedido.java
@@ -0,0 +1,75 @@
+package com.example.vegdog.model.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(optional = true)
+	private Cliente cliente;
+	
+	@ManyToMany
+	@Cascade(CascadeType.MERGE)
+	private List<Item> itens;
+	
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	private Date data;
+	
+	@Min(Long.MIN_VALUE)
+	private Double precoTotal;
+	
+	public Pedido() {}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<Item> getItens() {
+		return itens;
+	}
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
+	}
+
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Double getPrecoTotal() {
+		return precoTotal;
+	}
+	public void setPrecoTotal(Double precoTotal) {
+		this.precoTotal = precoTotal;
+	}
+
+}
