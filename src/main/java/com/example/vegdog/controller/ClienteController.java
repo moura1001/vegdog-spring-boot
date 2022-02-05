bfe971d3f76df310f229208bc156dc0489f8a560
moura1001 <gmouraneto01@gmail.com>
2022-02-05 12:37:11 -0300

    commit com adição da tela com listagem dos clientes cadastrados

---
+++ src/main/java/com/example/vegdog/controller/ClienteController.java
@@ -0,0 +1,24 @@
+package com.example.vegdog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.vegdog.model.entity.Cliente;
+import com.example.vegdog.model.repository.ClienteRepository;
+
+@Controller
+@RequestMapping("/clientes")
+public class ClienteController {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping("/")
+	public ModelAndView list() {
+		Iterable<Cliente> clientes = clienteRepository.findAll();
+		return new ModelAndView("cliente/list", "clientes", clientes);
+	}
+}
