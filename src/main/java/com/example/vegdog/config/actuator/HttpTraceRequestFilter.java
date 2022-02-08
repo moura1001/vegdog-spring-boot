b674bd0c2e3a3f1f8bedf07ed22bdc7861b879aa
moura1001 <gmouraneto01@gmail.com>
2022-02-08 10:04:06 -0300

    commit com configuração do Spring Actuator para monitoramento e gerenciamento da aplicação

---
+++ src/main/java/com/example/vegdog/config/actuator/HttpTraceRequestFilter.java
@@ -0,0 +1,23 @@
+package com.example.vegdog.config.actuator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.actuate.trace.http.HttpExchangeTracer;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.web.trace.servlet.HttpTraceFilter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpTraceRequestFilter extends HttpTraceFilter {
+
+	public HttpTraceRequestFilter(HttpTraceRepository repository, HttpExchangeTracer tracer) {
+		super(repository, tracer);
+	}
+	
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+		return request.getServletPath().contains("actuator");
+	}
+
+}
