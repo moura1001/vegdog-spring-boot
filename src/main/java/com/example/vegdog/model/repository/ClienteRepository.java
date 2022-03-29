1ed9c2e62a460b528df9d77f93a1c4ec93b65652
moura1001 <gmouraneto01@gmail.com>
2022-03-29 18:04:30 -0300

    commit com adição da possibilidade de busca de clientes por nome

--- src/main/java/com/example/vegdog/model/repository/ClienteRepository.java
+++ src/main/java/com/example/vegdog/model/repository/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.example.vegdog.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.vegdog.model.entity.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	Optional<Cliente> findByNome(String nome);
 
 }
