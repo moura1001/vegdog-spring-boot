72fd6ec81039a2184a0579c5f6d2ea1b9158148e
moura1001 <gmouraneto01@gmail.com>
2022-03-29 18:15:46 -0300

    commit com implementação da tela de pedios, utilizando Angular para consumir os serviços REST da API

---
+++ src/main/resources/static/js/delivery.js
@@ -0,0 +1,57 @@
+var app = angular.module("delivery", ["checklist-model"], function ($locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
+
+app.controller('pedidoController', function ($scope, $location, $http) {
+
+    $scope.itens = [];
+    $scope.subTotal = 0;
+    $scope.pedidoItens = [];
+
+    var carregarItens = function () {
+        $http.get("/api/itens").success(function (data) {
+            $scope.itens = data["_embedded"]["itens"];
+        }).error(function (data, status) {
+            $scope.message = "Aconteceu um problema: " + data;
+        });
+    };
+
+    $scope.realizarPedido = function (cliente, pedidoItens) {
+        $scope.message = "";
+        var itensId = [];
+        for (var i = 0; i < $scope.pedidoItens.length; i++)
+            itensId.push($scope.pedidoItens[i].id);
+        
+        $scope.urlPedido = "/rest/pedido/novo";
+
+        var data = {
+            "clienteNome" : cliente,
+            "itensId" : itensId
+        }
+
+        $http.post($scope.urlPedido, data).success(function (data) {
+            console.log(data["pedido"]);
+            $scope.idPedido = data["pedido"];
+            $scope.precoTotal = data["precoTotal"];
+            $scope.mensagem = data["mensagem"];
+        }).error(function (data, status) {
+            $scope.message = "Aconteceu um problema: "
+                + "Status:" + data.status + " - error:" + data.error;
+        });
+    };
+
+    $scope.selecionarItem = function () {
+
+        if (this.checked)
+            $scope.subTotal += this.i.preco;
+        else
+            $scope.subTotal -= this.i.preco;
+
+    }
+
+    carregarItens();
+
+});
