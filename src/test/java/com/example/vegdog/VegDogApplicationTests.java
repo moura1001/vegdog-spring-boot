54b2173c0b36ed75147aa4706a2e4321004930a4
moura1001 <gmouraneto01@gmail.com>
2022-03-31 16:24:12 -0300

    commit com adição de testes de integração para os serviços de busca de itens e novos pedidos

--- src/test/java/com/example/vegdog/VegDogApplicationTests.java
+++ src/test/java/com/example/vegdog/VegDogApplicationTests.java
@@ -1,13 +1,79 @@
 package com.example.vegdog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
+import static org.hamcrest.Matchers.*;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("dev")
 class VegDogApplicationTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void deveriaConterOServicoRESTParaClientes() {
+		try {
+			URI uri = new URI("/api");
+			
+			mockMvc
+			.perform(get(uri))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("clientes")));
+		} catch (Exception e) {
+			fail("Test failed: " + e.getMessage());
+		}
+	}
+	
+	@Test
+	void oPrecoDoPrimeiroItemCadastradoDeveriaSer8Reais() {
+		try {
+			URI uri = new URI("/api/itens/1");
+			
+			mockMvc
+			.perform(get(uri))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("preco", equalTo(8d)));
+		} catch (Exception e) {
+			fail("Test failed: " + e.getMessage());
+		}
+	}
+	
+	@Test
+	void deveriaCadastrarUmNovoPedido() {
+		try {
+			URI uri = new URI("/rest/pedido/novo");
+			String json = "{\"clienteNome\":\"João Maria\",\"itensId\":[1,2,3]}";
+			
+			mockMvc
+			.perform(post(uri)
+					.content(json)
+					.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("pedido", greaterThan(3)))
+			.andExpect(jsonPath("precoTotal", equalTo(56d)))
+			.andExpect(jsonPath("mensagem", equalTo("Pedido efetuado com sucesso")));
+		} catch (Exception e) {
+			fail("Test failed: " + e.getMessage());
+		}
 	}
 
 }
