dcf36f275243cf187c714a4f743b6f363e0b816a
moura1001 <gmouraneto01@gmail.com>
2022-04-05 10:58:45 -0300

    commit com alteração no sistema principal para integração ao Spring Cloud (registro no Eureka Server e comunicação com o Config Server)

--- vegdog-service/src/main/java/com/example/vegdog/VegDogApplication.java
+++ vegdog-service/src/main/java/com/example/vegdog/VegDogApplication.java
@@ -2,8 +2,10 @@ package com.example.vegdog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VegDogApplication {
 
 	public static void main(String[] args) {
