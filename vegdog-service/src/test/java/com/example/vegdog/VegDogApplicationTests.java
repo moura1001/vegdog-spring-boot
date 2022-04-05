aafa40fcd5e633ff8b071b121200ac01ad02ff4f
moura1001 <gmouraneto01@gmail.com>
2022-04-05 11:19:34 -0300

    commit com alteração na configuração de profiles ativos durante os testes de integração

--- vegdog-service/src/test/java/com/example/vegdog/VegDogApplicationTests.java
+++ vegdog-service/src/test/java/com/example/vegdog/VegDogApplicationTests.java
@@ -22,7 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("dev")
+@ActiveProfiles({"dev", "admin"})
 class VegDogApplicationTests {
 	
 	@Autowired
